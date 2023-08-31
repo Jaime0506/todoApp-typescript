ee776c40b1d3bdb93365932fa93ddc1efbc321b8
jaime0506 <jaimemejia1003@gmail.com>
2023-08-30 22:06:29 -0500

    start desing responsive

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -3,8 +3,8 @@ import { FormLogin } from "../components";
 
 export const LoginPage = () => {
     return (
-        <main className="bg-zinc-200 w-screen h-screen flex flex-col items-center justify-center">
-            <div className="bg-white w-1/2 rounded-md flex flex-col shadow-lg">
+        <main className="bg-zinc-200 w-screen h-screen flex flex-col items-center justify-center min-w-[300px]">
+            <div className="bg-white md:w-[450px] sm:w-[380px] w-3/4 min-w-[330px] rounded-md flex flex-col shadow-lg">
                 <header className="pt-6 pr-6 pl-6 pb-2">
                     <h1 className="font-bold">Todo App ✅</h1>
                 </header>
